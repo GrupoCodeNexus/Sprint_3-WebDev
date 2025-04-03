8ab1119ae590e49b41d91ad048d87de1a1a698bc
Francisco <franciscovargasmarcal@gmail.com>
2025-04-03 20:25:16 -0300

    Criando as rotas Error, Login e Home

---
+++ src/routes/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>
+      <h1>Error 404 - Página não encontrada</h1>
+      <p>preste atenção...</p>
+    </div>
+  )
+}
+
+export default Error

30f3cb7856b4afa2d6659fbf1ae6d69fec36838f
Francisco <franciscovargasmarcal@gmail.com>
2025-04-03 20:26:40 -0300

    Importando as rotas e componentes criados

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+// importando rotas e componentes necessárias
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from 'react'
+import Error from './routes/Error'
+import Login from './routes/Login'
+import Home from './routes/Home'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
 
-function App() {
-
+const App = () => {
   return (
     <>
+    <Router>
+      <Nav/>
+      <Routes>
+        <Route path="*" element={<Error/>}/>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/home" element={<Home/>}/>
+      </Routes>
+      <Footer/> 
+    </Router>
+    
+    
+    
     </>
-  )
+)
 }
 
 export default App

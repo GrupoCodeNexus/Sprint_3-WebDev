34d5c3c268e58596cf17741846b7a0baaf9ffc8f
Francisco <franciscovargasmarcal@gmail.com>
2025-04-03 21:40:11 -0300

    adicionando conteúdo ao footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <footer className='rodape'>
+      <h6>Todos os direitos reservados | 2025 - @CodeNexus</h6>
+    </footer>
+    
+    </>
   )
 }
 

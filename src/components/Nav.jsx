628acf46a47912ee3bf7537852903dd05ee62add
Francisco <franciscovargasmarcal@gmail.com>
2025-04-03 21:40:44 -0300

    adicionando conteúdo ao Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/style.css'
 
 const Nav = () => {
   return (
-    <div>
-      
+    <>
+    <div className="c-nav">
+      <Link to="/Home" className="nav-link">Home</Link>
     </div>
+    </>
   )
 }
 

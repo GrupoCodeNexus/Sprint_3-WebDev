a659659fee539253f551cffa1f3859d083f0b1c2
Francisco <franciscovargasmarcal@gmail.com>
2025-04-10 17:33:21 -0300

    Adicionando os novos components criados na Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/style.css'
+import logo from '../assets/loog.png'
 
 const Nav = () => {
   return (
     <>
     <div className="c-nav">
+      <img src={logo} alt="Logo" width='40px'/>
       <Link to="/Home" className="nav-link">Home</Link>
+      <Link to="/Problema" className="nav-link">Problema Abordado</Link>
+      <Link to="/Solucao" className="nav-link">Nossa Solução</Link>
     </div>
     </>
   )

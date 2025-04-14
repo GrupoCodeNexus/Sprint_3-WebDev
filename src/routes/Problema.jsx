b9be2337fed2a9c120156ccee2906eb74327764b
Francisco <franciscovargasmarcal@gmail.com>
2025-04-14 20:05:20 -0300

    criando sessão problema

--- src/routes/Problema.jsx
+++ src/routes/Problema.jsx
@@ -4,7 +4,19 @@ import React from 'react'
 const Problema = () => {
   return (
     <div>
-      <h1>Problema Abordado</h1>
+      <>
+      <main>
+        <div className='c-problema'>
+          <h2>Problema Abordado</h2>
+          <div className='problema'>
+            <p><span className='letra-n'>N</span>ós abordamos Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tempora alias dolorum cum, eaque velit blanditiis aut quidem
+             recusandae ipsa ea reprehenderit rerum necessitatibus libero corrupti quibusdam praesentium consectetur? Debitis.</p>
+          </div>
+        </div>
+      </main>
+
+      
+      </>
     </div>
   )
 }
